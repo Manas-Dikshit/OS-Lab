cd548e5d5be8cd3e9726afd14d764b1e65fcc842
Manas-Dikshit <manasdikshit48@gmail.com>
2026-09-22 10:44:38 +0530

    fix: correct semaphore and thread function macros for consistency and clarity

--- ProducerConsumer/C/producer_consumer.c
+++ ProducerConsumer/C/producer_consumer.c
@@ -1,14 +1,13 @@
 #ifdef _WIN32
 #include <windows.h>
 typedef HANDLE sem_t;
-#define sem_init(s, p, v) (*(s) = CreateSemaphore(NULL, v, 0x7FFFFFFF, NULL), 1)
-#define sem_wait(s) (WaitForSingleObject(*(s), INFINITE) == WAIT_OBJECT_0)
-#define sem_post(s) (ReleaseSemaphore(*(s), 1, NULL) != 0)
-#define sem_destroy(s) (CloseHandle(*(s)), 0)
+#define sem_init(s, p, v) ((void)((*(s) = CreateSemaphore(NULL, v, 0x7FFFFFFF, NULL)), 1))
+#define sem_wait(s) ((void)WaitForSingleObject(*(s), INFINITE))
+#define sem_post(s) ((void)ReleaseSemaphore(*(s), 1, NULL))
+#define sem_destroy(s) ((void)CloseHandle(*(s)))
 typedef HANDLE pthread_t;
-#define pthread_create(t, a, f, arg) \
-    (*(t) = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)(f), (arg), 0, NULL), *(t) != NULL)
-#define pthread_join(t, r) (WaitForSingleObject(t, INFINITE) == WAIT_OBJECT_0)
+#define pthread_create(t, a, f, arg) ((void)((*(t) = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)(f), (arg), 0, NULL)), 1))
+#define pthread_join(t, r) ((void)WaitForSingleObject(t, INFINITE))
 #else
 #include <pthread.h>
 #include <semaphore.h>
